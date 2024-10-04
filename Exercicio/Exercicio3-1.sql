fe85482735fe5a1e1c678ced8c024def73073a36
Juan Viana Lorenzo <47398092814@info.local>
2024-10-04 19:48:15 -0300

    Exercicio legal

--- Exercicio/Exercicio3-1.sql
+++ Exercicio/Exercicio3-1.sql
@@ -36,14 +36,22 @@ WHERE E.id_emprestimo IS NULL;
 
 -- 3
 
-SELECT * FROM tb_proprietario AS P
-LEFT JOIN tb_carros AS C
-ON P.id_carro = C.id_proprietario
+SELECT L.titulo, A.nome FROM tb_livros AS L
+LEFT JOIN tb_autores AS A
+ON L.id_autor = A.id_autor
 UNION
-SELECT * FROM tb_proprietario AS P
-RIGHT JOIN tb_carros AS C
-ON C.id_proprietario = P.id_carro
-WHERE P.id_carro IS NULL;
+SELECT L.titulo, A.nome FROM tb_autores AS A
+LEFT JOIN tb_livros AS L
+ON L.id_autor = A.id_autor;
+
+SELECT * FROM tb_membros AS M
+LEFT JOIN tb_emprestimos AS E
+ON M.id_membro = E.id_membro
+LEFT JOIN tb_livros AS L
+ON L.id_livro = E.id_livro
+WHERE id_membro IS NULL;
+
+
 
 
 
--- Exercicio/Exercicio3-2.sql
+++ Exercicio/Exercicio3-2.sql
@@ -1,3 +1,66 @@
 USE db_clinica;
 
 SELECT * FROM tb_veterinario;
+
+SELECT * FROM tb_cliente AS C
+INNER JOIN tb_pets AS P
+ON C.id_cliente = P.id_cliente;
+
+SELECT * FROM tb_veterinario AS V
+INNER JOIN tb_atendimento AS A
+ON V.id_veterinario = A.id_veterinario;
+
+SELECT * FROM tb_pets AS P
+INNER JOIN tb_atendimento AS A
+ON P.id_pet = A.id_pet
+INNER JOIN tb_veterinario AS V
+ON V.id_veterinario = A.id_veterinario;
+
+SELECT * FROM tb_atendimento AS A
+INNER JOIN tb_pets AS P
+ON A.id_pet = P.id_pet
+INNER JOIN tb_cliente AS C
+ON P.id_cliente = C.id_cliente
+INNER JOIN tb_veterinario AS V
+ON A.id_veterinario = V.id_veterinario
+WHERE A.data_nascimento = (SELECT MAX(data_nascimento) FROM tb_atendimento); -- Escrevi errado (SOU BURRO!) no lugar de nascimento vc escreve atendimento
+
+SELECT * FROM tb_atendimento AS A
+RIGHT JOIN tb_veterinario AS V
+ON A.id_veterinario = V.id_veterinario;
+
+SELECT * FROM tb_cliente AS C
+LEFT JOIN tb_pets AS P
+ON C.id_cliente = P.id_cliente;
+
+SELECT * FROM tb_cliente AS C
+LEFT JOIN tb_pets AS P
+ON P.id_cliente = C.id_cliente
+UNION
+SELECT * FROM tb_pets AS P
+RIGHT JOIN tb_pets AS P
+ON P.id_cliente = C.id_cliente;
+
+SELECT * FROM tb_atendimento AS A
+LEFT JOIN tb_pets AS P
+ON P.id_pet = C.id_pet
+UNION
+SELECT * FROM tb_atendimento AS A
+RIGHT JOIN tb_pets AS P
+ON P.id_pet = C.id_pet;
+
+SELECT * FROM tb_veterinario AS V
+LEFT JOIN tb_atendimento AS A
+ON V.id_veterinario = A.id_veterinario
+UNION
+SELECT * FROM tb_veterinario AS V
+RIGHT JOIN tb_atendimento AS A
+ON V.id_veterinario = A.id_veterinario;
+
+SELECT * FROM tb_atendimento AS A 
+LEFT JOIN tb_pets AS P
+ON A.id_pet = P.id_pet
+UNION
+SELECT * FROM tb_atendimento AS A 
+RIGHT JOIN tb_pets AS P
+ON A.id_pet = P.id_pet;
